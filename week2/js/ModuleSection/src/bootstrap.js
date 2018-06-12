6c6aa2345832a99b03b8a317023e06fbc249cb0e
Kardbord <tanner.kvarfordt@gmail.com>
2018-06-12 08:54:42 -0600

    Added js import demo

--- week2/js/ModuleSection/src/bootstrap.js
+++ week2/js/ModuleSection/src/bootstrap.js
@@ -1,1 +1,10 @@
-console.log('test');
+// import { greeting } from './helper';
+// import { multiply } from './helper';
+// import { greeting, multiply } from './helper';
+//
+// console.log(greeting);
+// console.log(multiply(2, 5));
+
+import * as helper from './helper';
+console.log(helper.greeting);
+console.log(helper.multiply(2, 5));
