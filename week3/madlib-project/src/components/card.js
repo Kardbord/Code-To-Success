a37ebab53a294c7be4b52a932e45a5537ca39622
Kardbord <tanner.kvarfordt@gmail.com>
2018-06-20 09:38:47 -0600

    Added some basic styles

---
+++ week3/madlib-project/src/components/card.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+
+import Input from "./input";
+
+class Card extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      color: ""
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  render() {
+    return (
+      <div className="card">
+        <h1>{this.state.color}</h1>
+        {Input("Color", this.state.color, this.handleInputChange, "color")}
+      </div>
+    );
+  }
+}
+
+export default Card;
---
+++ week3/madlib-project/src/components/input.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Input = (label, state, onChange, name) => {
+  return (
+    <div className="input">
+      <input name={name} value={state} onChange={onChange} />
+      <label>{label}</label>
+    </div>
+  );
+};
+
+export default Input;
